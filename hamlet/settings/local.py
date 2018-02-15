edf0f0044327c4a01c58f5ee716da9f4b17b5ab6
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-02-15 14:53:56 -0500

    Use .env rather than personal settings to toggle model in use

--- hamlet/settings/local.py
+++ hamlet/settings/local.py
@@ -8,11 +8,20 @@ ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1:8000']
 
 SECRET_KEY = 'f=fqwc&$zt_6rf8y45j1l7w!^e*%a_c)4sf+v*_uf%hwf5_*16'
 
-# This should be the full path of the neural net model to be used.
-# *Make sure this file is not .gitignored* for Heroku deployment. This means
-# it should not live in neural/nets, where the test models are written. Once
-# you have selected a test model for use, move it to your preferred location.
-MODEL_FILE = os.path.join(PROJECT_DIR, 'model', 'hamlet.model')
+# MODEL_FILE is the full path of the neural net model to be used.
+# *Make sure the test file is not .gitignored*; it is needed for CI.
+# However, production-quality models are too big for GitHub, so they should be
+# .gitignored.
+# MODEL_FILE defaults to the test model used for CI; because it is checked into
+# the repo it should be present and is therefore a sensible default for local
+# development. If you want to have a production-like environment, and to use
+# a model that represents the entire database, get it separately; put it in
+# hamlet/model/hamlet.model; and add DJANGO_USE_LIVE_MODEL=True to your .env.
+modelpath = os.environ.get('DJANGO_MODEL_path', '')
+if modelpath:
+    MODEL_FILE = os.path.join(PROJECT_DIR, modelpath)
+else:
+    MODEL_FILE = os.path.join(PROJECT_DIR, 'testmodels', 'testmodel.model')
 
 NEURAL_NET = Doc2Vec.load(MODEL_FILE)
 
