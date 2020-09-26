339cccbaaeaba62d2a582f2c5d1eeac1e224405a
Andromeda Yelton <andromeda.yelton@gmail.com>
2020-09-25 20:43:07 -0400

    Fix URL patterns the rest of the way

--- hamlet/theses/urls.py
+++ hamlet/theses/urls.py
@@ -7,9 +7,9 @@ app_name = 'theses'
 urlpatterns = [
     path('similar_to/',
         views.SimilarToSearchView.as_view(), name='similar_to'),
-    path('similar_to/(<int:identifier>)/',
+    path('similar_to/<int:identifier>/',
         views.SimilarToView.as_view(), name='similar_to'),
-    path('similar_to/author/(<int:pk>)/',
+    path('similar_to/author/<int:pk>/',
         views.SimilarToByAuthorView.as_view(), name='similar_to_by_author'),
     path('autocomplete/author/',
         views.AutocompleteAuthorView.as_view(), name='autocomplete_author'),
