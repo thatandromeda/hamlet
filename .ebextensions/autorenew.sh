47a2c0b43546b54ebb34ec1b82ea844999b6825e
Andromeda Yelton <andromeda.yelton@gmail.com>
2021-08-27 14:30:52 -0400

    give up on escaping quotes

---
+++ .ebextensions/autorenew.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
