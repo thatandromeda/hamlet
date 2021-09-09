2506b0ed3f482b30e1aebee9ab21d6c722c48131
Andromeda Yelton <andromeda.yelton@gmail.com>
2021-09-09 16:57:34 -0400

    Make sure there's only one autorenew job

--- .ebextensions/autorenew.sh
+++ .ebextensions/autorenew.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
+# Add autorenew script to crontab, but only if it isn't already there.
+if !(grep -F 'certbot renew' /etc/crontab) ; then
+  echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
+fi
