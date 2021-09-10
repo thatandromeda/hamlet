365680c0781315f1df4eaafc00a6660b193319ea
Andromeda Yelton <andromeda.yelton@gmail.com>
2021-09-10 11:28:34 -0400

    Try it with platform hooks

---
+++ .platform/hooks/predeploy/01_django_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# See https://github.com/aws/elastic-beanstalk-roadmap/issues/68 ,
+# https://github.com/aws/elastic-beanstalk-roadmap/issues/15 .
+
+source /var/app/venv/*/bin/activate
+cd /var/app/staging
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py compress
