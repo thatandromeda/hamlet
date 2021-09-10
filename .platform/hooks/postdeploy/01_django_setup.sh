8c6b5cf4ee68f5190103b312dd73e2f66e1dbe2f
Andromeda Yelton <andromeda.yelton@gmail.com>
2021-09-10 14:32:19 -0400

    goats goats goats

--- .platform/hooks/postdeploy/01_django_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# See https://github.com/aws/elastic-beanstalk-roadmap/issues/68 ,
-# https://github.com/aws/elastic-beanstalk-roadmap/issues/15 .
-
-source /var/app/venv/*/bin/activate
-cd /var/app/current
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py compress
